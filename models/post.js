169fe57df37e90709920bccdac7b61d0e57b565d
Poynter111 <alex.poynter111@gmail.com>
2018-04-13 09:19:29 +0100

    Small changes to layout

--- models/post.js
+++ models/post.js
@@ -5,7 +5,7 @@ const postSchema = new mongoose.Schema({
   location: String,
   date: Date,
   photos: [{type: mongoose.Schema.ObjectId, ref: 'Photo'}],
-  rating: {type: Number, min: 1, max: 5, default: 3}
+  rating: {type: Number, min: 1, max: 100, default: 50}
 });
 
 module.exports = mongoose.model('Post', postSchema);
