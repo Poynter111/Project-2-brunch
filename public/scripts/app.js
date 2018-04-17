8d17c5fa7df702688cdfa1ac15a8157fc308179f
Poynter111 <alex.poynter111@gmail.com>
2018-04-17 11:21:13 +0100

    More styling done, show page for reviews done

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,6 +2,16 @@ $(() => {
 
   $('.tabs').tabs(); //Tabs on reviews page
 
-  $('.datepicker').datepicker(); //date picker on New and edit forms
+  $('.datepicker').datepicker({
+    firstDay: 2
+  }); //date picker on New and edit forms
+
+  // const options = {
+  //   autoClose: true,
+  //   firstDay: 1
+  // };
+  //
+  // var elem = document.querySelector('.datepicker');
+  // M.Datepicker.init(elem, options);
 
 });
